3e9e9a491f8c30b441890c444962fbde5642b9ba
DEEPVYAS03 <113181349+DEEPVYAS03@users.noreply.github.com>
2024-06-20 15:29:47 +0530

    Delete trial directory

--- trial/Main.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Streamlit App", page_icon="🧊")
-
-st.title("Streamlit App")
--- trial/backend/routes/JobRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { postJobs } = require('../controller/JobController');
-
-router.post('/postJobs', postJobs);
-
-module.exports = router;
